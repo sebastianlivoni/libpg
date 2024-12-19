f1ad4f7da034e8fb0204db34111e40591e8e7093
Sebastian Livoni <29739749+sebastianlivoni@users.noreply.github.com>
2024-12-20 00:18:16 +0100

    Update password_generator.c
    compability with windows

--- src/password_generator.c
+++ src/password_generator.c
@@ -1,17 +1,19 @@
 #include "include/password_generator.h"
 
 char *generate_password() {
+  srand(time(NULL) + clock());
+
   int num_positions[] = {5, 7, 12, 14, 19};
   int cap_let_positions[] = {0,  1,  2,  3,  4,  5,  7,  8,  9,
                              10, 11, 12, 14, 15, 16, 17, 18, 19};
 
-  int num_pos = arc4random_uniform(5);
-  int cap_pos = arc4random_uniform(18);
+  int num_pos = rand() % 5;
+  int cap_pos = rand() % 18;
 
   if (num_positions[num_pos] == cap_let_positions[cap_pos])
     cap_pos++;
 
-  char *pass = calloc(PASSWORD_LENGTH, sizeof(char));
+  char *pass = malloc(PASSWORD_LENGTH, sizeof(char));
 
   int j = 0;
   for (int i = 0; i < PASSWORD_LENGTH; i++) {
@@ -19,17 +21,19 @@ char *generate_password() {
       pass[i] = SEPARATOR;
       j = 0;
     } else if (i == num_positions[num_pos]) {
-      pass[i] = NUMBERS[arc4random_uniform(NUMBERS_LENGTH)];
+      pass[i] = NUMBERS[rand() % NUMBERS_LENGTH];
     } else if (j == 1 || j == 4) {
-      pass[i] = VOWELS[arc4random_uniform(VOWELS_LENGTH)];
+      pass[i] = VOWELS[rand() % VOWELS_LENGTH];
       j++;
     } else {
-      pass[i] = CONSONANTS[arc4random_uniform(CONSONANTS_LENGTH)];
+      pass[i] = CONSONANTS[rand() % CONSONANTS_LENGTH];
       j++;
     }
   }
 
   pass[cap_pos] = toupper(pass[cap_pos]);
+
+  pass[PASSWORD_LENGTH] = '\0';
 
   return pass;
 }
